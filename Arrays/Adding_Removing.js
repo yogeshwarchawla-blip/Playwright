c1ee4d4e029d479fea9065a7392aece098488638
yoges <yoges@localhost>
2026-03-13 14:34:06 +0530

    Auto commit: 2026-03-13T14:34:06.6245749+05:30

---
+++ Arrays/Adding_Removing.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3]
+console.log(arr);
+
+// Adding Array 
+arr.push(4);
+console.log(arr);
+
+// Removing array 
+arr.pop();
+console.log(arr);
---
+++ Arrays/Array_creation.js
@@ -0,0 +1,3 @@
+// let fruit = []     // empty array 
+let numbers = [1, 2, 3]
+console.log(numbers[2]);
